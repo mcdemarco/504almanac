8537cff2f76978a811f28324cd0d7f8d0295de9e
M. C. DeMarco <mcd@mcdemarco.net>
2016-01-23 18:11:07 -0500

    Add links and hash history.

---
+++ src/constants/Names.jsx
@@ -0,0 +1,44 @@
+export const names = [
+	[
+		"The World of Traveling",
+		"The World of Restless",
+		"The World of Individual",
+		"The World of Combative",
+		"The World of Exploring",
+		"The World of Networking",
+		"The World of the Mightiest",
+		"The World of Productive",
+		"A World of Publicly Owned",
+		"A World of ____",
+		"A World of ____",
+		"A World of ____"
+	],
+	[
+		"Traders",
+		"Pioneers",
+		"Industrialists",
+		"Generals",
+		"Explorers",
+		"Road Builders",
+		"Big Landowners",
+		"Businessmen",
+		"Stock Companies",
+		"___",
+		"___",
+		"___"
+	],
+	[
+		"on Journeys",
+		"Pressed for Time",
+		"with a Bias to Individualism",
+		"with Potential for Violence",
+		"on the Way to the Unknown",
+		"with Connections",
+		"Craving for Recognition",
+		"with Production Needs",
+		"by External Financing",
+		"___",
+		"___",
+		"___"
+	]
+]

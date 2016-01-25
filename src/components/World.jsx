ad4e70f24e779fc557585d9764cf7f1520090dc4
M. C. DeMarco <mcd@mcdemarco.net>
2016-01-25 01:27:31 -0500

    Refactor links (still not happy).

--- src/components/World.jsx
+++ src/components/World.jsx
@@ -3,34 +3,47 @@ import {height, count} from '../constants/Layout';
 import {names} from '../constants/Names';
 import {links} from '../constants/2FLinks';
 
+const LinkList = React.createClass({
+
+  getRulesLink(wN) {
+		{/*You can only get rules for a real world (no x's).*/}
+		return <li><a href={"http://504rules.github.io/#" + wN}>Get rules for world {wN} at 504rules</a></li>;
+	},
+
+  getRatingsLink(w,wN) {
+		{/*You can link to the ratings site with a real world or partial world but not a weird world (no x's followed by digits).*/}
+		const link = "http://504-2f.de/?page_id=" + links[parseInt(wN,10)];
+		return <li><a href={link}>Rate world {wN} at the official 504 site</a></li>;
+	},
+	
+	render() {
+		const { world, worldNo, worldType } = this.props;
+		return <ul>
+			{worldType == "weird" ? "" : this.getRatingsLink(world, worldNo)}
+			{worldType == "full" ? this.getRulesLink(worldNo) : ""}
+			<li><a href={"https://boardgamegeek.com/tag/504%3A"  + worldNo + "/"}>Search for world {worldNo} at BoardGameGeek</a></li>
+		</ul>;
+	}
+});
+
 const World = React.createClass({
 
   convertToWorldName(w) {
 		return w.reduce( (prev, valu, indx) => prev + " " + names[indx][valu], "");
 	},
-	
-  getBGGLink(wN) {
-		{/*You can search for any tag.*/}
-		const link = "https://boardgamegeek.com/tag/504%3A" + wN + "/";
-		return <li><a href={link}>Search for world {wN} at BoardGameGeek</a></li>;
-	},
-  getRulesLink(w,wN) {
-		{/*You can only get rules for a real world (no x's).*/}
-		const link = "http://504rules.github.io/#" + wN;
+
+	typeWorld(w) {
 		if (w[0] < 9 && w[1] < 9 && w[2] < 9)
-			return <li><a href={link}>Get rules for world {wN} at 504rules</a></li>;
-	},
-  getRatingsLink(w,wN) {
-		{/*You can link to the ratings site with a real world or partial world but not a weird world (no x's followed by digits).*/}
-		const link = "http://504-2f.de/?page_id=" + links[parseInt(wN,10)];
-		if (w[0] < 9 && w[1] < 9 && w[2] < 9)
-			return <li><a href={link}>Rate world {wN} at the official 504 site</a></li>;
+			return "full";
 		else if ( (w[0] < 9 && w[1] < 9) || (w[0] < 9 && w[2] > 8)  || (w[1] > 8 && w[2] > 8) )
-			return <li><a href={link}>Look up world {wN} at the official 504 site</a></li>;
+			return "partial";
+		else
+			return "weird";
 	},
 	
   render () {
 		const { world, worldNo } = this.props;
+		const worldType = this.typeWorld(world);
 		if (history.pushState) {
 			history.pushState(null, null, '#' + worldNo);
 		} else {
@@ -40,11 +53,7 @@ const World = React.createClass({
 		return <div className="worldDetails" style={{marginTop: height * count/3 + 20}}>
 			<h1>{"World " + worldNo}</h1>
 			<h2>{this.convertToWorldName(world)}</h2>
-			<ul>
-				{this.getRatingsLink(world, worldNo)}
-				{this.getRulesLink(world, worldNo)}
-				{this.getBGGLink(worldNo)}
-			</ul>
+			<LinkList world={world} worldNo={worldNo} worldType={worldType} />
 		</div>;
   }
 });

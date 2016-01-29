dc5ac3de2fe8d63a3439491cddd67d99e31fa1ba
M. C. DeMarco <mcd@mcdemarco.net>
2016-01-28 19:06:13 -0500

    Fix flex and iphone.

--- src/components/World.jsx
+++ src/components/World.jsx
@@ -8,13 +8,13 @@ const LinkList = React.createClass({
 
   getRulesLink(wN) {
 		{/*You can only get rules for a real world (no x's).*/}
-		return <li><a href={"http://504rules.github.io/#" + wN}>Get rules for world {wN} at 504rules</a></li>;
+		return <li><a href={"http://504rules.github.io/#" + wN} title="Get rules for this world at 504rules">Rules</a></li>;
 	},
 
   getRatingsLink(w,wN) {
 		{/*You can link to the ratings site with a real world or partial world but not a weird world (no x's followed by digits).*/}
 		const link = "http://504-2f.de/?page_id=" + links[parseInt(wN,10)];
-		return <li><a href={link}>Rate world {wN} at the official 504 site</a></li>;
+		return <li><a href={link} title="Rate this world at the official 504 site">Ratings</a></li>;
 	},
 	
 	render() {
@@ -22,7 +22,7 @@ const LinkList = React.createClass({
 		return <ul>
 			{worldType == "weird" ? "" : this.getRatingsLink(world, worldNo)}
 			{worldType == "full" ? this.getRulesLink(worldNo) : ""}
-			<li><a href={"https://boardgamegeek.com/tag/504%3A"  + worldNo + "/"}>Search for world {worldNo} at BoardGameGeek</a></li>
+			<li><a href={"https://boardgamegeek.com/tag/504%3A"  + worldNo + "/"} title="Search for this world at BoardGameGeek">BGG</a></li>
 		</ul>;
 	}
 });
@@ -54,8 +54,7 @@ const World = React.createClass({
 		} else {
 			location.hash = '#' + worldNo;
 		}
-		{/* was previously on the left with style marginLeft: width * 3 + 20 */}
-		return <div className="worldDetails" style={{marginTop: height * count/3 + 20}}>
+		return <div className="worldDetails">
 			<h1>{"World " + worldNo}</h1>
 			<h2>{this.convertToWorldName(world)}</h2>
 			<h3>{this.convertToWorldModules(world)}</h3>
